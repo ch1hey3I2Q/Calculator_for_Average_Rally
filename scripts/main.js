cd1f3f42f41aa27484feeebbf4c1b90facc2263d
ch1hey3I2Q <kametomo0624@gmail.com>
2026-04-20 19:07:10 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const datas = {};
-const output_sec = {"output1":null, "output2-1":null, "output2-2":null, "output2-3":null};
-const input_errer = {"time1":false, "time2":false, "time3":false, "distance":false, "speed1":false, "speed2":false};
+const output_sec = {"output1-1":null, "output1-2":null, "output2-1":null, "output2-2":null, "output2-3":null};
+const input_error = {"time1":false, "time2":false, "time3":false, "time4":false, "distance":false, "speed1":false, "speed2":false};
 
 const inputs = document.getElementsByClassName("inputs");
 for (let i = 0; i < inputs.length; i++) {
@@ -11,18 +11,24 @@ for (let i = 0; i < inputs.length; i++) {
             case "time1":
             case "time2":
             case "time3":
+            case "time4":
                 if (e.target.value !== '') {
-                    e.target.value = ('000000' + Number(e.target.value.replace(/[^(0-9)]+/g, ''))).slice(-6);
-                    const hour = Number(String(e.target.value.substring(0, 2))) <= 23;
-                    const minute = Number(String(e.target.value.substring(2, 4))) <= 59;
-                    const second = Number(String(e.target.value.substring(4, 6))) <= 59;
+                    const value = ('000000' + Number(e.target.value.replace(/[^(0-9)]+/g, ''))).slice(-6);
+                    const hour = e.target.name === "time1" || e.target.name === "time2" ? Number(String(value.substring(0, 2))) <= 23 : Number(String(value.substring(0, 2))) === 0;
+                    const minute = e.target.name === "time1" || e.target.name === "time2" ? Number(String(value.substring(2, 4))) <= 59 : true;
+                    const second = Number(String(value.substring(4, 6))) <= 59;
                     
                     if (hour && minute && second) {
-                        input_errer[e.target.name] = false;
-                        datas[e.target.name] = to_Sec(e.target.value);
+                        input_error[e.target.name] = false;
+                        datas[e.target.name] = to_Sec(value);
                         e.target.style.color = "black";
+                        if (e.target.name === "time1" || e.target.name === "time2") {
+                            e.target.value = value;
+                        } else {
+                            e.target.value = String(value.substring(2, 6));
+                        }
                     } else {
-                        input_errer[e.target.name] = true;
+                        input_error[e.target.name] = true;
                         datas[e.target.name] = null;
                         e.target.style.color = "red";
                     }
@@ -32,7 +38,12 @@ for (let i = 0; i < inputs.length; i++) {
                 break;
             case "distance":
                 e.target.value = Number(e.target.value.replace(/[^(0-9|.)]+/g, ''));
-                datas[e.target.name] = e.target.value;
+                if (e.target.value > 0){
+                    datas[e.target.name] = e.target.value;
+                } else {
+                    e.target.value = '';
+                    datas[e.target.name] = null;
+                }
                 break;
             case "speed1":
             case "speed2":
@@ -81,41 +92,75 @@ function to_hhmmss(sec, mode="h,m,s") {
     }
 }
 
+const output1_1 = document.getElementById("output1-1");
+const output1_2 = document.getElementById("output1-2");
+
 function output(name) {
     switch (name) {
+        case "reset":
         case "time1":
         case "time2":
         case "time3":
-            const output = document.getElementById("output1");
-            if (datas["time1"] !== null && datas["time2"] !== null && !(input_errer["time1"] || input_errer["time2"] || input_errer["time3"])) {
-                output_sec["output1"] = datas["time1"] - datas["time2"] - datas["time3"];
+            // const output1_1 = document.getElementById("output1-1");
+            // const output1_2 = document.getElementById("output1-2");
+            if (datas["time1"] !== null && datas["time2"] !== null && !(input_error["time1"] || input_error["time2"] || input_error["time3"])) {
+                output_sec["output1-1"] = datas["time1"] - datas["time2"] - datas["time3"];
                 let text = '';
 
-                if (output_sec["output1"] >= 0) {
-                    output.style.color = "blue";
+                if (output_sec["output1-1"] >= 0) {
+                    output1_1.style.color = "blue";
                 } else {
-                    output_sec["output1"] = output_sec["output1"] * -1;
-                    output.style.color = "red";
+                    output_sec["output1-1"] = output_sec["output1-1"] * -1;
+                    output1_1.style.color = "red";
                     text += '-';
                 }
 
-                const [minute, second] = to_hhmmss(output_sec["output1"], "m,s");
+                const [minute, second] = to_hhmmss(output_sec["output1-1"], "m,s");
                 text += minute + "分" + second + "秒";
                 
-                output.textContent = text;
-            } else if (input_errer["time1"] || input_errer["time2"] || input_errer["time3"]) {
-                output.textContent = 'Errer';
-                output.style.color = "red";
+                output1_1.textContent = text;
             } else {
-                output.textContent = '--';
-                output.style.color = "blue";
+                output_sec["output1-1"] = null;
+                if (input_error["time1"] || input_error["time2"] || input_error["time3"]) {
+                    output1_1.textContent = 'Error';
+                    output1_1.style.color = "red";
+                    if (datas["time4"] !== null) {
+                        // const output1_2 = document.getElementById("output1-2");
+                        output1_2.textContent = 'Error';
+                        output1_2.style.color = "red";
+                    }
+                } else {
+                    output1_1.textContent = '--';
+                    output1_1.style.color = "blue";
+                }
+            } 
+                
+        case "time4":
+            // const output1_2 = document.getElementById("output1-2");
+            if (datas["time4"] !== null && !input_error["time4"] && output_sec["output1-1"] !== null) {
+                output_sec["output1-2"] = Math.abs(output_sec["output1-1"] - datas["time4"]);
+                let text = '';
+
+                const [minute, second] = to_hhmmss(output_sec["output1-2"], "m,s");
+                text += minute + "分" + second + "秒";
+                
+                output1_2.textContent = text;
+            } else {
+                if (input_error["time4"]) {
+                    output1_2.textContent = 'Error';
+                    output1_2.style.color = "red";
+                } else {
+                    output1_2.textContent = '--';
+                    output1_2.style.color = "blue";
+                }
             }
+            break;
         case "distance":
         case "speed1":
         case "speed2":
-            const output1 = document.getElementById("output2-1");
-            const output2 = document.getElementById("output2-2");
-            const output3 = document.getElementById("output2-3");
+            const output2_1 = document.getElementById("output2-1");
+            const output2_2 = document.getElementById("output2-2");
+            const output2_3 = document.getElementById("output2-3");
             if (datas["distance"] !== null) {
                 if (name !== "speed2") {
                     if (datas["speed1"] !== null && datas["speed1"] !== 0) {
@@ -123,10 +168,10 @@ function output(name) {
                         const [minute, second] = to_hhmmss(output_sec["output2-1"], "m,s");
                         
                         const text = minute + "分" + second + "秒";
-                        output1.textContent = text;
+                        output2_1.textContent = text;
                     } else {
                         output_sec["output2-1"] = null;
-                        output1.textContent = '--';
+                        output2_1.textContent = '--';
                     }
                 }
 
@@ -137,10 +182,10 @@ function output(name) {
                         
                         const text = minute + "分" + second + "秒";
                         
-                        output2.textContent = text;
+                        output2_2.textContent = text;
                     } else {
                         output_sec["output2-2"] = null;
-                        output2.textContent = '--';
+                        output2_2.textContent = '--';
                     }
                 }
 
@@ -149,24 +194,47 @@ function output(name) {
                     let text = '';
 
                     if (output_sec["output2-3"] >= 0) {
-                        output3.style.color = "blue";
+                        output2_3.style.color = "blue";
                     } else {
                         output_sec["output2-3"] = output_sec["output2-3"] * -1;
-                        output3.style.color = "red";
+                        output2_3.style.color = "red";
                         text += '-';
                     }
 
                     const [minute, second] = to_hhmmss(output_sec["output2-3"], "m,s");
                     
                     text += minute + "分" + second + "秒";
-                    output3.textContent = text;
+                    output2_3.textContent = text;
                 } else {
-                    output3.textContent = '--';
-                    output3.style.color = "blue";
+                    output2_3.textContent = '--';
+                    output2_3.style.color = "blue";
                 }
             }
+            break;
     }
 }
+
+const btn_next = document.getElementById("btn_next");
+btn_next.addEventListener('click', () => {
+    if (!(input_error["time1"] || input_error["time2"] || input_error["time3"] || input_error["time4"])) {
+        const input_time1 = document.getElementById("input_time1");
+        const input_time2 = document.getElementById("input_time2");
+        const input_time3 = document.getElementById("input_time3");
+        const input_time4 = document.getElementById("input_time4");
+
+        input_time2.value = input_time1.value;
+        input_time1.value = '';
+        input_time3.value = '';
+        input_time4.value = '';
+
+        datas["time2"] = datas["time1"];
+        datas["time1"] = null;
+        datas["time3"] = null;
+        datas["time4"] = null;
+
+        output("reset");
+    }
+})
 
 const input = document.querySelectorAll("input");
 for (let i = 0; i < input.length; i++) {
@@ -178,3 +246,6 @@ for (let i = 0; i < input.length; i++) {
         }
     })
 }
+
+
+// レスコンカード入力でEnterで次へ行かないように できてる...?
